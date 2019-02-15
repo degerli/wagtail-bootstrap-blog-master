53a225802efe7cb6de8b8739c8e988d0563fea91
Mazen <m.shanti@windowslive.com>
2019-02-15 22:22:10 +0200

    Changing to use production settings

--- wagtail_tuto/settings/production.py
+++ wagtail_tuto/settings/production.py
@@ -9,6 +9,11 @@ try:
 except ImportError:
     pass
 
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -18,3 +23,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
--- wagtail_tuto/wsgi.py
+++ wagtail_tuto/wsgi.py
@@ -1,3 +1,4 @@
+'''
 import os
 import sys
 
@@ -12,3 +13,12 @@ sys.path.append(os.path.join(app_path, 'wagtail_tuto'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings.dev")
 
 application = get_wsgi_application()
+'''
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_title.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
