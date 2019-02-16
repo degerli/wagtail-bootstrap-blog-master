66a6efb792d390a5952bde023af889ea16626134
Mazen <m.shanti@windowslive.com>
2019-02-16 23:25:39 +0200

    start

--- wagtail_tuto/wsgi.py
+++ wagtail_tuto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_title project.
+WSGI config for wagtail_tuto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_title.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
