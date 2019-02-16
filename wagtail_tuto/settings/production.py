ec8f16f9b47b74b736d64ea85a1224bc327bb093
Dia Saleh <diasalehs@gmail.com>
2019-02-17 00:54:18 +0200

    white

--- wagtail_tuto/settings/production.py
+++ wagtail_tuto/settings/production.py
@@ -23,3 +23,12 @@ ALLOWED_HOSTS = ['*']
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
--- wagtail_tuto/wsgi.py
+++ wagtail_tuto/wsgi.py
@@ -1,14 +1,8 @@
 import os
-import sys
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-# This allows easy placement of apps within the interior
-# djmyblog directory.
-app_path = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), os.pardir))
-sys.path.append(os.path.join(app_path, 'wagtail_tuto'))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_tuto.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
